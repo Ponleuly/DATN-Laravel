65703f4c8e32e5ad29d4fda6956b5edb28fbc535
Ponleuly <lyponleu116@gmail.com>
2023-08-08 22:22:50 +0700

    update product details information

--- resources/views/frontend/product/product_detail.blade.php
+++ resources/views/frontend/product/product_detail.blade.php
@@ -458,15 +458,15 @@
                                     <div class="collapse" id="collapsebank3">
                                         <div class="px-5">
                                             <ul>
-                                                <li>Chỉ đổi hàng 1 lần duy nhất, mong bạn cân nhắc kĩ trước khi quyết định.</li>
-                                                <li>Thời hạn đổi sản phẩm khi mua trực tiếp tại cửa hàng là 07 ngày, kể từ ngày mua. Đổi sản phẩm khi mua online là 14 ngày, kể từ ngày nhận hàng.</li>
-                                                <li>Sản phẩm đổi phải kèm hóa đơn. Bắt buộc phải còn nguyên tem, hộp, nhãn mác.</li>
-                                                <li>Sản phẩm đổi không có dấu hiệu đã qua sử dụng, không giặt tẩy, bám bẩn, biến dạng.</li>
-                                                <li>Ananas chỉ ưu tiên hỗ trợ đổi size. Trong trường hợp sản phẩm hết size cần đổi, bạn có thể đổi sang 01 sản phẩm khác:
-                                                    <br>- Nếu sản phẩm muốn đổi ngang giá trị hoặc có giá trị cao hơn, bạn sẽ cần bù khoảng chênh lệch tại thời điểm đổi (nếu có).
-                                                    <br>- Nếu bạn mong muốn đổi sản phẩm có giá trị thấp hơn, chúng tôi sẽ không hoàn lại tiền.</li>
-                                                <li>Trong trường hợp sản phẩm - size bạn muốn đổi không còn hàng trong hệ thống. Vui lòng chọn sản phẩm khác.</li>
-                                                <li>Không hoàn trả bằng tiền mặt dù bất cứ trong trường hợp nào. Mong bạn thông cảm.</li>
+                                                <li>Exchange only 1 time, please consider carefully before deciding.</li>
+                                                <li>The time limit for exchanging products when buying directly at the store is 07 days from the date of purchase. Product exchange when purchased online is 14 days from the date of receipt.</li>
+                                                <li>Exchange products must be accompanied by an invoice. Must have original stamps, boxes and labels.</li>
+                                                <li>The exchanged product has no signs of being used, not washed, stained, or deformed.</li>
+                                                <li>Shop only prioritizes support for size change. In case the product is out of size and needs to be exchanged, you can exchange it for another product:
+                                                    <br>- If the product you want to exchange is equal in value or has a higher value, you will need to compensate the difference at the time of exchange (if any).
+                                                    <br>- If you wish to exchange for a product of a lower value, we will not issue a refund.</li>
+                                                <li>In case the product - size you want to exchange is out of stock in the system. Please choose another product.</li>
+                                                <li>No cash refund under any circumstances. Wish you sympathize.</li>
                                             </ul>
                                         </div>
                                     </div>
@@ -483,14 +483,12 @@
                                     <div class="collapse" id="collapsebank4">
                                         <div class="px-5">
                                             <p class="mb-0">
-                                                Mỗi đôi giày Ananas trước khi xuất xưởng đều trải qua nhiều khâu kiểm tra.
-                                                Tuy vậy, trong quá trình sử dụng, nếu nhận thấy các lỗi: gãy đế, hở đế,
-                                                đứt chỉ may,...trong thời gian 6 tháng từ ngày mua hàng,
-                                                mong bạn sớm gửi sản phẩm về Ananas nhằm giúp chúng tôi có cơ hội phục vụ bạn tốt hơn.
-                                                Vui lòng gửi sản phẩm về bất kỳ cửa hàng Ananas nào,
-                                                hoặc gửi đến trung tâm bảo hành Ananas ngay trong trung tâm TP.HCM trong giờ hành chính:
-                                                Địa chỉ: 170-172, Đinh Bộ Lĩnh, P.26 , Q.Bình Thạnh, TP.HCM
-                                                Hotline: 028 2211 0067
+                                                Each pair of 15Steps shoes before being shipped goes through many stages of testing. 
+                                                However, during use, if you notice errors: 
+                                                broken sole, open sole, broken sewing thread, ... within 6 months from the date of purchase, 
+                                                hope you soon send the product to 15Steps to help us have the opportunity to serve you better. 
+                                                Please send the product to any 15Steps store, or send it to 15Steps warranty center right in the center of Ho Chi Minh City during office hours: 
+                                                Address: 170-172, Dinh Bo Linh, Ward 26, Dist. Binh Thanh, HCMC Hotline: 028 2211 0067.
                                             </p>
                                         </div>
                                     </div>
